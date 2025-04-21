5cb67690970283fddd0c7472e6984531a6011964
sandipmalii <21121a0567@vidyanikethan.asia>
2025-04-21 12:19:52 +0530

    Solved Final Error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   webpack: (config) => {
+//     config.resolve.fallback = { fs: false }; // Ignore 'fs' module in the client
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
+
+
+// next.config.js
+
+
+// next.config.js
+
+const nextConfig = {
   webpack: (config) => {
-    config.resolve.fallback = { fs: false }; // Ignore 'fs' module in the client
+    config.resolve.fallback = { fs: false }; // Ignore 'fs' module in client
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
